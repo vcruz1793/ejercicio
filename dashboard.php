37f4a69ab2c1f4ba94812dcb9c4c729ea069821a
victor <victorcruz1793@gmail.com>
2022-03-22 22:05:05 -0600

    Estilos para Dashboard

--- dashboard.php
+++ dashboard.php
@@ -4,11 +4,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Dashboard</title>
+	<link rel="stylesheet" href="assets/css/estilos.css">
 </head>
 <body>
-
-
-	<h1>Dashboard - Inicio</h1>
 
 	<div id="pgside">
 		<img src="assets/img/wolf_icon.png" alt="admin-panel-icon">
