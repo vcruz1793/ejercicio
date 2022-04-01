ddad47753c58b336fc0284ab0552cd240e8fda40
victor <victorcruz1793@gmail.com>
2022-03-31 21:39:11 -0600

    Vista de usuarios

--- dashboard.php
+++ dashboard.php
@@ -9,9 +9,58 @@
 <body>
 
 	<div id="pgside">
-		<img src="assets/img/wolf_icon.png" alt="admin-panel-icon">
-		<i class="txt">My Admin</i>
+
+		<div id="pguser">
+			<img src="assets/img/wolf2.jpg" alt="admin-panel-icon">
+			<i class="txt">My Admin</i>
+		</div>
+
+		<a href="#" class="current">
+			<i class="ico">&#9733;</i>
+			<i class="txt">Usuarios</i>
+		</a>
+
+		<a href="#" class="">
+			<i class="ico">&#9728;</i>
+			<i class="txt">Ventas</i>
+		</a>
+
+		<a href="#" class="">
+			<i class="ico">&#9737;</i>
+			<i class="txt">Reportes</i>
+		</a>				
+
 	</div>
+
+
+	<main id="seccion_principal">
+		<h1>Lista de usuarios</h1>
+
+		<table border="2">
+			<tr> 
+				<th>Nombre</th> 
+				<th>Dirección</th>
+				<th>Correo</th>
+				<th>Status</th>
+			</tr>
+			<tr>
+				<td>Julio</td>
+				<td>Balboa #516</td>
+				<td>julio@gmail.com</td>
+				<td>Activo</td>
+			</tr>
+			<tr>
+				<td>Erick</td>
+				<td>Vizcainas #116</td>
+				<td>Erick@gmail.com</td>
+				<td>Eliminado</td>
+			</tr>			
+		</table>
+
+
+	</main>
+
+
 
 </body>
 </html>
